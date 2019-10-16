661aa2cc4aee0e5dacda5ac4b8f2193268619d29
Agustin Azuaje <sygnus900@gmail.com>
2019-10-16 05:38:17 -0700

    Version 1.0

---
+++ main.js
@@ -0,0 +1,6 @@
+//Get the numbers from the user
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+//print the numbers in the html document
+document.write(parseFloat(num1) + parseFloat(num2));
